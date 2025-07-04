838e981a810e099c016388c6f9886960c4896c9f
Alessio Grespi <56415370+AlessioGrespi@users.noreply.github.com>
2025-07-04 21:50:59 +0100

    navbar compinent

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let data: { user: { id: number; email: string } | null };
+</script>
+
+<nav class="w-full border-neutral-200">
+  <div class="flex justify-between items-center  container mx-auto">
+    <div class="flex items-center gap-4">
+      <a href="/">Home</a>
+    </div>
+    <div class="flex items-center gap-4">
+      <a href="/protected">Protected</a>
+      <a href="/protected/settings">Settings</a>
+    </div>
+    <div class="flex items-center gap-4">
+      {#if data.user}
+        <a href="/logout">Logout</a>
+      {:else}
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+      {/if}
+    </div>
+  </div>
+</nav>
