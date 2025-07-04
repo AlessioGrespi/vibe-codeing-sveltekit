6ea76bd0d7f0af150085008c0e62f6d50f7038e7
Alessio Grespi <56415370+AlessioGrespi@users.noreply.github.com>
2025-07-04 17:05:06 +0100

    navbar

---
+++ src/routes/protected/+layout.svelte
@@ -0,0 +1,9 @@
+<nav class='flex gap-4'>
+    <a href="/">Home</a>
+    <a href="/login">Login</a>
+    <a href="/register">Register</a>
+    <a href="/logout">Logout</a>
+    <a href="/protected">Protected</a>
+</nav>
+
+<slot />
